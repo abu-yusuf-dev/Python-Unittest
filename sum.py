88b7e39fbc9836ef526020732fa78d982538dc46
Revel Yusuf <crazyprogrammer109@gmail.com>
2017-07-22 20:41:27 +0600

    Python Unittest
    Here the basic of Python Unittest by Pytest.

---
+++ sum.py
@@ -0,0 +1,5 @@
+def sum(a,b):
+    return a+b
+
+def sum2(a,b):
+    return a*b
---
+++ test_sum.py
@@ -0,0 +1,11 @@
+import sum
+def test_sum():
+    total=sum.sum(4,5)
+    assert total==9
+
+def test_sum2():
+    total=sum.sum2(4,5)
+    assert total==20
+
+
+
